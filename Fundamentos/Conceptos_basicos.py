e2ae50b372bc189b9ceab2298335d72300fbd3f3
zeroprotec <zero.protec@gmail.com>
2022-04-29 12:08:14 -0500

    Rename Conceptos Basicos

---
+++ Fundamentos/Conceptos_basicos.py
@@ -0,0 +1,104 @@
+
+#Tipos de datos
+
+numero1 = 1
+print(numero1)
+print(type(numero1))
+
+#float o flotante se refiere a un numero decimal
+numero2 = 1.5
+print(numero1)
+print(type(numero2))
+
+#str o string se refiere a una cadena de texto
+texto1 = "hola"
+print(texto1)
+print(type(texto1))
+
+#bolean o buleano se refiere a una afirmacion o necagion como false and true
+buleano1 = False
+print(buleano1)
+print(type(buleano1))
+
+#declarar variables numericas tipo int()
+variable1 = int(1)
+print(variable1)
+print(type(variable1))
+#declarar variebles de tipo float
+
+variable2 = (2.0)
+print(variable2)
+print(type(variable2))
+#declarar variable de tipo string
+
+variable3 = ("hoal mundo")
+print (variable3)
+print (type(variable3))
+
+#declarar conversion de variable tipo int
+a = 1
+print(a)
+print(type(a))
+
+a = int(a)
+print (a)
+print(type(a))
+
+#operaciones aritmeticas potenciacion
+b = 2
+print(b)
+print(type(b))
+print(b**3)
+#operaciones aritmeticas modulo
+c = 20
+print(c)
+print(c%5)
+#opreciones aritmeticas igualdad
+x = 5
+y = 5
+z = x == y
+print(x == y)
+print(type(z))
+
+#operaciones aritmeticas diferencia
+d = 5
+f = 6
+print(d != f)
+
+#operaciones aritmeticas mayor que
+g = 20
+h = 30
+print(g > h)
+
+#operaciones aritmeticas menor que
+i = 10
+j = 11
+print(i < j)
+
+# operaciones aritmeticas mayor o igual que
+k = 10
+print (k >= 50)
+
+# operaciones aritmeticas menor o igual que
+k = 10
+print (k <= 50)
+
+#operador and es parecido a la ley de signos
+print (True and False)
+#false
+
+#operador OR o "y" sirver para concatenar dos expresiones
+l = 5
+m = 5
+print (l > 5 or l == 5)
+
+#operador NOT o no devbuelve el valor inverso
+print (not False)
+# True 
+#funcion input
+dato_ob = input()
+print(dato_ob)
+#prueba
+dato_in = input()
+dato_in = dato_in + 5
+print(dato_in)
