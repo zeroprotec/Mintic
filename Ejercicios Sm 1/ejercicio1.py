74269d2e5902021eda0b4f29859aa8df8b5b7b5a
zeroprotec <zero.protec@gmail.com>
2022-04-29 18:36:24 -0500

    Ejercicios Semana 1

---
+++ Ejercicios Sm 1/ejercicio1.py
@@ -0,0 +1,8 @@
+# Realizar conversion de temperatura Celsius a Faherenheit
+
+temperaturaUs = input ("Hola, escribe la temperatura ")
+temperaturaUs = float()
+temperaturaF = float(1.8 * temperaturaUs + 32)
+print ("La temperatura en Faherenheit es ")
+print (temperaturaF)
+
