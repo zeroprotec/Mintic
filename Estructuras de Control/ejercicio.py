30b2e54b8bd266dfb39275f5d92c3fd1a0f8d27d
zeroprotec <zero.protec@gmail.com>
2022-05-04 21:21:41 -0500

    Actualizado

---
+++ Estructuras de Control/ejercicio.py
@@ -0,0 +1,10 @@
+#Solucion problema cuestionario
+#Para saber la respuesta correcta correr el programa en esta direccion : mintic/Estructuras de Control 
+a = 18
+b = a%5
+if( a == b):
+    print("Son iguales")
+elif (a>b):
+    print("el primer número es mayor que el segundo")
+else:
+    print("el segundo número es mayor que el primero")
